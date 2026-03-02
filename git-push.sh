8933611842fa781f1ab4b855bb1b478d5dd452d1
Tudulius <tudulius@gmail.com>
2026-03-03 08:50:12 +0900

    git push

---
+++ git-push.sh
@@ -0,0 +1,13 @@
+# …or create a new repository on the command line
+# echo "# Kakao4-Github-Actions-S3" >> README.md
+# git init
+git add .
+git commit -m "first commit"
+# git branch -M main
+# git remote add origin https://github.com/tudulius/Kakao4-Github-Actions-S3.git
+git push -u origin main
+
+# …or push an existing repository from the command line
+# git remote add origin https://github.com/tudulius/Kakao4-Github-Actions-S3.git
+# git branch -M main
+# git push -u origin main
